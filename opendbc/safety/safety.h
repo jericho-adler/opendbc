3739a9ac338d71c6d01c5b9013139d508fd580ea
Paper1 <paper1@outlook.pt>
2025-08-25 06:05:01 +0100

    WIP

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -403,6 +403,7 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_TESLA, &tesla_hooks},
     {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
 #ifdef ALLOW_DEBUG
+    {SAFETY_VOLVO, &volvo_hooks},
     {SAFETY_PSA, &psa_hooks},
     {SAFETY_SUBARU_PREGLOBAL, &subaru_preglobal_hooks},
     {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
