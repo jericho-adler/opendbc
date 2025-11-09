b24e32189bd9ae2f30d19893c7935d4ae997f0c1
Paper1 <paper1@outlook.pt>
2025-11-09 19:49:00 +0000

    WIP Volvo CMA

--- opendbc/safety/safety.h
+++ opendbc/safety/safety.h
@@ -402,8 +402,8 @@ int set_safety_hooks(uint16_t mode, uint16_t param) {
     {SAFETY_RIVIAN, &rivian_hooks},
     {SAFETY_TESLA, &tesla_hooks},
     {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
-#ifdef ALLOW_DEBUG
     {SAFETY_VOLVO, &volvo_hooks},
+#ifdef ALLOW_DEBUG
     {SAFETY_PSA, &psa_hooks},
     {SAFETY_SUBARU_PREGLOBAL, &subaru_preglobal_hooks},
     {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
