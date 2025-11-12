b888e855225563ef47887eb8724800cbcc293124
Paper1 <paper1@outlook.pt>
2025-11-12 18:00:07 +0000

    WIP Volvo CMA

--- opendbc/car/volvo/volvocan.py
+++ opendbc/car/volvo/volvocan.py
@@ -40,4 +40,21 @@ def create_lca_steering(packer, lat_active: bool, apply_torque: int):
     'NEW_SIGNAL_6': 15, # ?
   }
 
+  if not lat_active:
+    values = {
+      'NEW_SIGNAL_3': 0,
+      'LCA_ENABLE_INV': 1,
+      'NEW_SIGNAL_1': 3,
+      'LCA_STEER_LOOSELY_1': 0,
+      'LCA_STEER_ACTIVE_INCOHERENT': 0,
+      'LCA_STEER_ACTIVE': 0,
+      'NEW_SIGNAL_7': 7,
+      'LCA_STEER_LOOSELY_2': 0,
+      'NEW_SIGNAL_4': 251,
+      'CURVE_RIGHT': 0,
+      'NEW_SIGNAL_5': 3,
+      'LCA_STEER': 0,
+      'NEW_SIGNAL_6': 15,
+    }
+
   return packer.make_can_msg('LCA', 2, values)
