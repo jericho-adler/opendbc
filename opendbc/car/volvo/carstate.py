807a5db42c210e467c3ae3022d10e29bc6a08d72
Paper1 <paper1@outlook.pt>
2025-11-13 07:44:54 +0000

    WIP Volvo CMA

--- opendbc/car/volvo/carstate.py
+++ opendbc/car/volvo/carstate.py
@@ -59,7 +59,7 @@ class CarState(CarStateBase):
     #ret.brakePressed = bool(cp_main.vl["VCU1"]["BRAKE_PEDAL_PRESSED_A"] or cp_main.vl["VCU1"]["BRAKE_PEDAL_PRESSED_B"])
     # BRAKE_PEDAL_PRESSED_A goes active when user starts pressing brake pedal, but no brake light is on yet due to tolerance
     # BRAKE_PEDAL_PRESSED_B goes active when when the brake pedal is pressed above minimum threshold, brake light is on
-    ret.brakePressed = cp_pt.vl["VCU1"]["BRAKE_PEDAL_PRESSED_B"] == 1
+    ret.brakePressed = cp_main.vl["VCU1"]["BRAKE_PEDAL_PRESSED_B"] == 1
     ret.parkingBrake = False # TODO: add parking brake
 
     # steering wheel
