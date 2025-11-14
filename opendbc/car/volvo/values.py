e56f245c566725a9a539ebe0658d73ec39cb96d6
Paper1 <paper1@outlook.pt>
2025-11-14 19:52:17 +0000

    WIP Volvo CMA: Increase steerRatio

--- opendbc/car/volvo/values.py
+++ opendbc/car/volvo/values.py
@@ -50,7 +50,7 @@ class CAR(Platforms):
     CarSpecs(
       mass=2170,
       wheelbase=2.702,
-      steerRatio=25.0,  # steerRatio 15.8, increased to 25.0 because openpilot is autolearning to ~30
+      steerRatio=30.0,  # steerRatio 15.8, increased to 30.0 because openpilot is autolearning to ~30 (if not higher)
       centerToFrontRatio=0.52,
     ),
   )
