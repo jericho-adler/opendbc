3a4c5b05e43953928695e76358f9e0f046bb620c
Paper1 <paper1@outlook.pt>
2025-11-16 19:07:38 +0000

    WIP Volvo CMA

--- opendbc/car/volvo/carstate.py
+++ opendbc/car/volvo/carstate.py
@@ -66,6 +66,7 @@ class CarState(CarStateBase):
     ret.cruiseState.enabled = cruise_raw and self.cruise_double_tap_active
     self.cruise_enabled_prev = cruise_raw
 
+    ret.cruiseState.enabled = cruise_raw # No more double-tap detection, uncomment if needed
     ret.cruiseState.available = True  # TODO: Determine actual availability
     ret.cruiseState.speed = 0  # TODO: Find cruise set speed (not required for lateral control)
     ret.cruiseState.nonAdaptive = False
