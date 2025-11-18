5d868a72460246a232ecd352aa1cf131a592d285
Paper1 <paper1@outlook.pt>
2025-11-18 16:25:45 +0000

    WIP Volvo CMA

--- opendbc/car/volvo/carcontroller.py
+++ opendbc/car/volvo/carcontroller.py
@@ -51,7 +51,7 @@ class CarController(CarControllerBase):
     # LCA_2 - 0x69 - 50 Hz
     # Spoof PILOT_ASSIST_ENGAGED to keep PSCM accepting LCA commands
     if self.frame % 2 == 0: # 50 Hz
-      can_sends.append(create_lca_2_message(self.packer, CC.latActive, CS.msg_lca_2))
+      #can_sends.append(create_lca_2_message(self.packer, CC.latActive, CS.msg_lca_2))
       #self.lca_2_counter_1_prev = int(CS.msg_lca_2['COUNTER_1'])
       pass
 
@@ -64,9 +64,10 @@ class CarController(CarControllerBase):
 
     # SPEED messages - 0x60, 0x67, 0x68 - 50 Hz
     if self.frame % 2 == 0: # 50 Hz
-      can_sends.append(create_speed_3_message(self.packer, CS.msg_speed_3))
-      can_sends.append(create_speed_1_message(self.packer, CS.msg_speed_1))
-      can_sends.append(create_speed_2_message(self.packer, CS.msg_speed_2))
+      #can_sends.append(create_speed_3_message(self.packer, CS.msg_speed_3))
+      #can_sends.append(create_speed_1_message(self.packer, CS.msg_speed_1))
+      #can_sends.append(create_speed_2_message(self.packer, CS.msg_speed_2))
+      pass
 
     new_actuators = actuators.as_builder()
     new_actuators.torque = self.apply_torque_last / CarControllerParams.STEER_MAX
