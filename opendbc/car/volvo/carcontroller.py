b75f99ed13ffc5749f5e1c94fa42987f236228d1
Paper1 <paper1@outlook.pt>
2025-11-18 16:34:13 +0000

    WIP Volvo CMA

--- opendbc/car/volvo/carcontroller.py
+++ opendbc/car/volvo/carcontroller.py
@@ -51,7 +51,7 @@ class CarController(CarControllerBase):
     # LCA_2 - 0x69 - 50 Hz
     # Spoof PILOT_ASSIST_ENGAGED to keep PSCM accepting LCA commands
     if self.frame % 2 == 0: # 50 Hz
-      #can_sends.append(create_lca_2_message(self.packer, CC.latActive, CS.msg_lca_2))
+      can_sends.append(create_lca_2_message(self.packer, CC.latActive, CS.msg_lca_2))
       #self.lca_2_counter_1_prev = int(CS.msg_lca_2['COUNTER_1'])
       pass
 
