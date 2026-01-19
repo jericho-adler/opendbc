81e6f2499fc442aab4f91e30e5e11336ce7b3fff
Paper1 <paper1@outlook.pt>
2026-01-19 17:25:20 +0000

    Disable GEAR_POSITION message

--- opendbc/car/volvo/carcontroller.py
+++ opendbc/car/volvo/carcontroller.py
@@ -179,8 +179,8 @@ class CarController(CarControllerBase):
     #if self.gear_acc >= 100:
     #    self.gear_acc -= 100
     if self.frame % 5 == 0 or self.frame % 5 == 2:  # 2/5 * 100 Hz = 40 Hz # openpilot forward delay causes DTC in EGSM, but fixes DTC in PSCM
-      can_sends.append(create_gear_position_message(self.packer, CS.msg_gear_position))
-      #pass
+      #can_sends.append(create_gear_position_message(self.packer, CS.msg_gear_position))
+      pass
 
     new_actuators = actuators.as_builder()
     new_actuators.steeringAngleDeg = self.apply_angle_last
