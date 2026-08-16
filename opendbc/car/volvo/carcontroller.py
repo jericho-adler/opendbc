b6834e16b8561630a83aa1fd3d92e45693f04ce8
Thomas Fifer <thomasfiferjr@gmail.com>
2026-08-16 15:16:15 -0400

    clean up hands on wheel spoof
    figured out the pscm message a bit better for the dbc, update code to reflect changes and properly spoof hands on wheel

--- opendbc/car/volvo/carcontroller.py
+++ opendbc/car/volvo/carcontroller.py
@@ -209,9 +209,10 @@ class CarController(CarControllerBase):
 
       # Check if PA hands-on-wheel spoof toggle is enabled (bit 7 of alternativeExperience)
       spoof_pa_hands_enabled = bool(self.CP.alternativeExperience & 128)
-      spoof_pa_hands = CS.pilot_assist_engaged and spoof_pa_hands_enabled
+      spoof_hands_on_wheel = lat_active or (CS.pilot_assist_engaged and spoof_pa_hands_enabled)
+
       # PSCM (bus 2 -> 0) - 0x16 - 100 Hz
-      can_sends.append(create_pscm_message(self.packer, lat_active, CS.msg_pscm, self.frame, spoof_pa_hands))
+      can_sends.append(create_pscm_message(self.packer, CS.msg_pscm, spoof_hands_on_wheel))
       # EGSM - 0x45 - 100 Hz
       #can_sends.append(create_egsm_message(self.packer, CS.msg_egsm))
 
